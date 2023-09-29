7f487ba2c2b1081bc41b553fd2e49b6a816a4c32
Crmy7 <charlesremy03@gmail.com>
2023-09-29 11:55:45 +0200

    final edits

--- src/main.js
+++ src/main.js
@@ -58,10 +58,103 @@ $('.interior-container').each(function () {
         opacity: 0,
         scrollTrigger: {
             trigger: this,
-            start: 'top 125%',
+            start: 'top 155%',
             end: 'bottom center',
             scrub: 1,
             toggleActions: 'restart none none reverse',
         },
     });
 });
+
+
+
+let isVisible = false;
+let timeoutId = null;
+
+$('.menu-icon-toggle').on('click', function (e) {
+    $('body').toggleClass('open');
+
+    if (!isVisible) {
+        isVisible = true;
+        $('.wrapper-menu').css({
+            visibility: 'visible',
+            opacity: 1
+        });
+    } else {
+        isVisible = false;
+        setTimeout(function () {
+            $('.wrapper-menu').css('visibility', 'hidden');
+            $('.wrapper-menu').css('opacity', '0');
+        }, 1000);
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+    }
+
+    e.preventDefault();
+});
+
+
+
+
+$('.menu-mobile li').each(function (index) {
+    var delay = (index + 1) * 0.1;
+    $(this).css('transition-delay', delay + 's');
+    $(this).find('span').css('transition-delay', delay + 's');
+});
+
+
+var currentURL = window.location.href;
+
+// Parcourir les éléments <a> dans la liste
+$('.menu li a').each(function () {
+    var href = $(this).attr('href');
+
+    // Comparer l'URL de la page avec l'attribut href de l'élément <a>
+    if (href === currentURL) {
+        // Ajouter la classe "current-menu-item" à l'élément <li> parent
+        $(this).parent().addClass('current-menu-item');
+    }
+});
+
+function moveTool(elem) {
+    if (elem) {
+        var thisa = elem.querySelector("a");
+        var x = thisa.offsetLeft + thisa.offsetWidth - (thisa.offsetWidth / 2);
+        var triangle = document.querySelector(".menubottri");
+        if (triangle) {
+            triangle.style.left = x + "px";
+        }
+    }
+}
+
+function handleHover(event) {
+    var target = event.target;
+    var li = target.closest("li");
+    if (li) {
+        moveTool(li);
+    }
+}
+var currentMenuItem = document.querySelector('.menu li.current-menu-item');
+moveTool(currentMenuItem);
+
+var menuItems = document.querySelectorAll('.menu li');
+menuItems.forEach(function (item) {
+    item.addEventListener('mouseenter', handleHover);
+});
+
+
+// Sélectionnez l'élément .nav
+const nav = document.querySelector('header.header nav');
+
+// Fonction pour changer la couleur de fond de l'en-tête en blanc lorsque l'utilisateur fait défiler
+function changeNavBackground() {
+    if (window.scrollY > 50) { // Vous pouvez ajuster le seuil selon vos besoins
+        nav.style.backgroundColor = 'white';
+    } else {
+        nav.style.backgroundColor = 'transparent';
+    }
+}
+
+// Écoutez l'événement de défilement de la fenêtre
+window.addEventListener('scroll', changeNavBackground);
